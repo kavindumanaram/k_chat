b519956ffe633fd6974532c5a8f6d2e78ab66a3b
Kavindu Manaram <kavinduxyz@gmail.com>
2016-05-27 23:21:17 +0530

    Update connection.php

--- model/connection.php
+++ model/connection.php
@@ -1,7 +1,7 @@
 <?php
 class connection{
     function  query($sql){
-        $conn=  mysql_connect('sql311.0fees.us','0fe_15420609','kavindu999k') 
+        $conn=  mysql_connect('YOUR_SERVER_NAME','USERNAME','PASSWORD') 
                 or die("Could not connect server");
         $db=  mysql_select_db('0fe_15420609_chat_box',$conn) 
                 or die("Could not connect database");
