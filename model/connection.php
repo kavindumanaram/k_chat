47927502825dd5a4985316a82d67d99af1c13560
Kavindu Manaram <kavinduxyz@gmail.com>
2016-05-27 23:22:06 +0530

    Update connection.php

--- model/connection.php
+++ model/connection.php
@@ -3,7 +3,7 @@ class connection{
     function  query($sql){
         $conn=  mysql_connect('YOUR_SERVER_NAME','USERNAME','PASSWORD') 
                 or die("Could not connect server");
-        $db=  mysql_select_db('0fe_15420609_chat_box',$conn) 
+        $db=  mysql_select_db('DATABASE_NAME',$conn) 
                 or die("Could not connect database");
         $result = mysql_query($sql) or die("Could not connect query");
         return $result;
